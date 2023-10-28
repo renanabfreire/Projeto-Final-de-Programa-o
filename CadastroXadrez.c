d5e82ea5fd3373462951acebfe58e3dac68a9bb3
Moriancumer02 <143050197+Moriancumer02@users.noreply.github.com>
2023-10-27 23:02:22 -0300

    Update CadastroXadrez.c

--- CadastroXadrez.c
+++ CadastroXadrez.c
@@ -3,7 +3,6 @@
 #include <time.h> //biblioteca para manipular tempo 
 #include <unistd.h> //biblioteca para funções como 'sleep()' !
 
-//inicio do codigo
 //estrutura de representar os players
 struct player {
     char nome[20], sobrenome[20], sexo, pais[20];
